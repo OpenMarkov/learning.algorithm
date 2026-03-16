59168174bf45262644c22a204e18626cb637fa48
manuel <marias@dia.uned.es>
2026-03-16 23:11:01 +0100

    Fix EditHistorySupportTest: create a minimal ProbNet to instantiate AddLinkEdit
      Both tests were calling new AddLinkEdit(null, null, null, ...), which throws
      a NullPointerException. testMarkAndCheck silenced it with a try-catch,
      effectively testing nothing. testReset propagated the exception and errored.
      Fixed by creating a minimal ProbNet with two variables in @BeforeEach.

--- src/test/java/org/openmarkov/learning/algorithm/nbderived/common/EditHistorySupportTest.java
+++ src/test/java/org/openmarkov/learning/algorithm/nbderived/common/EditHistorySupportTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.openmarkov.core.action.base.PNEdit;
 import org.openmarkov.core.action.base.linkEdits.AddLinkEdit;
 import org.openmarkov.core.action.base.linkEdits.BaseLinkEdit;
+import org.openmarkov.core.model.network.NodeType;
+import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.core.model.network.Variable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,48 +18,40 @@ public class EditHistorySupportTest {
 
     private List<PNEdit> backingList;
     private EditHistorySupport support;
+    private ProbNet probNet;
+    private Variable varA;
+    private Variable varB;
 
     @BeforeEach
     public void setup() {
         backingList = new ArrayList<>();
         support = new EditHistorySupport(backingList);
+
+        probNet = new ProbNet();
+        varA = new Variable("A", 2);
+        varB = new Variable("B", 2);
+        probNet.addNode(varA, NodeType.CHANCE);
+        probNet.addNode(varB, NodeType.CHANCE);
     }
 
     @Test
     public void testMarkAndCheck() {
-        // Use AddLinkEdit with nulls. Assuming constructor doesn't enforce non-null for
-        // bare instantiation or we catch runtime errors.
-        // If constructor throws, we can't easily test without ProbNet.
-        // But we are mainly testing the 'history' logic (list operations).
-        // Since we can't create DummyEdit (sealed), we try this.
-        BaseLinkEdit edit1 = null;
-        try {
-            edit1 = new AddLinkEdit(null, null, null, true);
-        } catch (Exception e) {
-            // If it fails, we can't proceed with this strategy easily.
-            // But let's assume for now it might work or we just want to verify compilation.
-        }
+        BaseLinkEdit edit1 = new AddLinkEdit(probNet, varA, varB, true);
+        BaseLinkEdit edit2 = new AddLinkEdit(probNet, varA, varB, false);
 
-        // If instantiation failed (edit1 is null), logic below throws.
-        // We really need an object that satisfies BaseLinkEdit type.
-        // If we can't create one, we can't test it.
-        if (edit1 != null) {
-            BaseLinkEdit edit2 = new AddLinkEdit(null, null, null, false); // different object
+        assertFalse(support.isEditAlreadyConsidered(edit1));
 
-            assertFalse(support.isEditAlreadyConsidered(edit1));
+        support.markEditAsConsidered(edit1);
 
-            support.markEditAsConsidered(edit1);
-
-            assertTrue(support.isEditAlreadyConsidered(edit1));
-            assertFalse(support.isEditAlreadyConsidered(edit2));
-            assertEquals(1, backingList.size());
-            assertEquals(edit1, backingList.getFirst());
-        }
+        assertTrue(support.isEditAlreadyConsidered(edit1));
+        assertFalse(support.isEditAlreadyConsidered(edit2));
+        assertEquals(1, backingList.size());
+        assertEquals(edit1, backingList.getFirst());
     }
 
     @Test
-    public void testReset() throws Exception {
-        BaseLinkEdit edit1 = new AddLinkEdit(null, null, null, true);
+    public void testReset() {
+        BaseLinkEdit edit1 = new AddLinkEdit(probNet, varA, varB, true);
 
         support.markEditAsConsidered(edit1);
         assertTrue(support.isEditAlreadyConsidered(edit1));
